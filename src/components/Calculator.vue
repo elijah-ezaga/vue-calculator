c3aa09d9f3061931bb6b1f2f99a83de8fecb2014
Elijah <elijah.ezaga@gmail.com>
2018-10-26 16:57:32 +0200

    Performed refactorings to fix issues: a) keyboard input appearing twice after pressing + b) operating calculator from keyboard (in progress)

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,28 +1,42 @@
 <template>
     <div id="calculator">
         <div id="io-display">
-            <input type="text" v-model="result" />
+            <input type="text" v-model="result"
+                @keypress.96="processNumber('0')"
+                @keypress.97="processNumber('1')"
+                @keypress.98="processNumber('2')"
+                @keypress.99="processNumber('3')"
+                @keypress.100="processNumber('4')"
+                @keypress.101="processNumber('5')"
+                @keypress.102="processNumber('6')"
+                @keypress.103="processNumber('7')"
+                @keypress.104="processNumber('8')"
+                @keypress.105="processNumber('9')"
+
+                @keypress.enter.prevent="processEqual"
+                @keypress.107.prevent="processPlus"
+            />
         </div>
         <div id="buttons-panel">
             <div class="panel-row">
-                <input type="button" value="1" @click="checkInput('1')" />
-                <input type="button" value="2" @click="checkInput('2')" />
-                <input type="button" value="3" @click="checkInput('3')" />
+                <input type="button" value="1" @click="processNumber('1')" />
+                <input type="button" value="2" @click="processNumber('2')" />
+                <input type="button" value="3" @click="processNumber('3')" />
             </div>
             <div class="panel-row">
-                <input type="button" value="4" @click="checkInput('4')" />
-                <input type="button" value="5" @click="checkInput('5')" />
-                <input type="button" value="6" @click="checkInput('6')" />
+                <input type="button" value="4" @click="processNumber('4')" />
+                <input type="button" value="5" @click="processNumber('5')" />
+                <input type="button" value="6" @click="processNumber('6')" />
             </div>
             <div class="panel-row">
-                <input type="button" value="7" @click="checkInput('7')" />
-                <input type="button" value="8" @click="checkInput('8')" />
-                <input type="button" value="9" @click="checkInput('9')" />
+                <input type="button" value="7" @click="processNumber('7')" />
+                <input type="button" value="8" @click="processNumber('8')" />
+                <input type="button" value="9" @click="processNumber('9')" />
             </div>
             <div class="panel-row">
-                <input type="button" value="0" @click="checkInput('0')" />
-                <input type="button" value="+" @click="checkInput('+')" />
-                <input type="button" value="=" @click="checkInput('=')" />
+                <input type="button" value="0" @click="processNumber('0')" />
+                <input type="button" value="+" @click="processPlus" />
+                <input type="button" value="=" @click="processEqual" />
             </div>
             <div class="panel-row">
                 <input type="button" value="C" v-on:click="clearInput" />
@@ -47,29 +61,28 @@ export default {
         }
     },
     methods: {
-        checkInput: function(value) {
-            if (value === '+') {
-                this.performCalculation = true;
-                if (this.lastInputIsNumber) {
-                    this.buffer += parseFloat(this.result);
-                    this.result = this.buffer;
-                }
-
-                this.lastInputIsNumber = false;
-            } else if ( value >= 0 || value <= 9) {
-                this.updateInput(value);
-                this.lastInputIsNumber = true;
-            } else if (value === '=') {
-                this.result = this.buffer + parseFloat(this.result);
-                this.buffer = ZERO;
-                this.lastInputIsNumber = false;
+        processPlus: function() {
+            this.performCalculation = true;
+            if (this.lastInputIsNumber) {
+                this.buffer += parseFloat(this.result);
+                this.result = this.buffer;
             }
+
+            this.lastInputIsNumber = false;
         },
 
-        updateInput: function(value) {
+        processEqual: function() {
+            this.result = this.buffer + parseFloat(this.result);
+            this.buffer = ZERO;
+            this.lastInputIsNumber = false;
+        },
+
+        processNumber: function(value) {
             if (this.performCalculation) {
                 this.result = value;
                 this.performCalculation = false;
+                this.lastInputIsNumber = true;
+                return;
             }
 
             if (value === EMPTY || parseFloat(this.result) === ZERO) {
@@ -77,6 +90,8 @@ export default {
             } else {
                 this.result += value;
             }
+
+            this.lastInputIsNumber = true;
         },
 
         clearInput: function() {
